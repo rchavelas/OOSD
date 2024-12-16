66442832175faa61d108f027219841d6fddf423f
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2024-12-16 22:24:57 +0100

    tested variable names with _

--- Ex3_Teacup.py
+++ Ex3_Teacup.py
@@ -1,13 +1,22 @@
-"""Teacup model PYSD"""
+"""Example 3: Original Teacup model from the PYSD tutorial"""
 from OOSD import SdModel
 
-sdm = SdModel(name="Teacup", start="", stop="", dt="")
+sdm = SdModel(name="Teacup", start=0, stop=30.0, dt=0.125)
 
-sdm.add_stock(name = "Teacup Temperature", eqn="", outflow="Heat Loss to Room")
+# Add stocks to main model
+#sdm.add_stock(name="Teacup Temperature", eqn="180", outflow="'Heat Loss to Room'")
+sdm.add_stock(name="Teacup_Temperature", eqn="180", outflow="Heat_Loss_to_Room")
 
-sdm.add_flow(name="Heat Loss to Room", eqn="")
+# Add flows to main model
+#sdm.add_flow(name="Heat Loss to Room", 
+#             eqn="('Teacup Temperature'-'Room Temperature')/'Characteristic Time'")
+sdm.add_flow(name="Heat_Loss_to_Room", 
+             eqn="(Teacup_Temperature - Room_Temperature) / Characteristic_Time")
 
-sdm.add_auxiliary(name="Characteristic Time", eqn="")
-sdm.add_auxiliary(name="Room Temperature", eqn="")
+# Add auxiliaries to main model
+#sdm.add_auxiliary(name="Characteristic Time", eqn=10)
+#sdm.add_auxiliary(name="Room Temperature", eqn=70)  
+sdm.add_auxiliary(name="Characteristic_Time", eqn=10)
+sdm.add_auxiliary(name="Room_Temperature", eqn=70)  
 
 print(sdm)
